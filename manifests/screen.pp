9a56202a111687e6e3c48a57749f74447e0d508c
Justin Bronn <justin@counsyl.com>
2013-02-17 16:34:12 -0800

    Added the `sys::screen` package.

---
+++ manifests/screen.pp
@@ -0,0 +1,34 @@
+# == Class: sys::screen
+#
+# Installs GNU screen, the terminal multiplexing tool.
+#
+# === Parameters
+#
+# These paramaters are for advanced usage only.
+#
+# [*ensure*]
+#  The ensure value to use for the screen package.  Defaults to 'installed'.
+#
+# [*package*]
+#  The name of the package to install; the default depens on the OS, most
+#  likely it is 'screen'.
+#
+# [*provider*]
+#  The provider to use for the package; the default is undef.
+#
+# [*source*]
+#  The source to use for the package; the default is undef.
+#
+class sys::screen(
+  $ensure   = 'installed',
+  $package  = $sys::screen::params::package,
+  $provider = $sys::screen::params::provider,
+  $source   = $sys::screen::params::source,
+) inherits sys::screen::params {
+  package { $package:
+    ensure   => $ensure,
+    alias    => 'screen',
+    provider => $provider,
+    source   => $source,
+  }
+}
---
+++ manifests/screen/params.pp
@@ -0,0 +1,21 @@
+# == Class: sys::screen::params
+#
+# Platform-dependent parameters for installing screen.
+#
+class screen::params {
+  case $::osfamily {
+    openbsd: {
+      include sys::openbsd
+      $package = 'screen'
+      $source = $sys::openbsd::pkg::source
+    }
+    solaris: {
+      include sys::solaris
+      $package = 'terminal/screen'
+      $provider = 'pkg'
+    }
+    default: {
+      $package = 'screen'
+    }
+  }
+}
