0e00112cd2689c0e490097467826c3ce3727bbaf
Justin Bronn <justin@counsyl.com>
2013-02-17 16:26:42 -0800

    Added `$illumos` parameter.

--- manifests/solaris.pp
+++ manifests/solaris.pp
@@ -4,27 +4,31 @@
 #
 class sys::solaris {
   if $::osfamily != 'Solaris' or versioncmp($::kernelrelease, '5.11') < 0 {
-    fail("The sys::solaris module only for Solaris 11+ and Illumos variants.\n")
+    fail('Only Solaris 11+ and Illumos kernels are supported.\n')
   }
 
   # Set parameters based on the Solaris kernel version.  Each variant
   # has different paths.
   case $::kernelversion {
     /^omnios/: {
+      $illumos = true
       $omnios = true
-      $path = "/opt/omni/bin/amd64:/opt/omni/bin:/usr/gnu/bin/amd64:/usr/gnu/bin:/usr/bin/amd64:/usr/bin:/usr/sbin/amd64:/usr/sbin:/sbin"
+      $path = '/opt/omni/bin/amd64:/opt/omni/bin:/usr/gnu/bin/amd64:/usr/gnu/bin:/usr/bin/amd64:/usr/bin:/usr/sbin/amd64:/usr/sbin:/sbin'
     }
     /^oi/: {
+      $illumos = true
       $openindiana = true
-      $path = "/usr/gnu/bin/amd64:/usr/gnu/bin:/usr/bin/amd64:/usr/bin:/usr/sbin/amd64:/usr/sbin:/sbin"
+      $path = '/usr/gnu/bin/amd64:/usr/gnu/bin:/usr/bin/amd64:/usr/bin:/usr/sbin/amd64:/usr/sbin:/sbin'
     }
     /^joyent/: {
+      $illumos = true
       $smartos = true
-      $path = "/usr/bin:/usr/sbin:/smartdc/bin:/opt/local/bin:/opt/local/sbin"
+      $path = '/usr/bin:/usr/sbin:/smartdc/bin:/opt/local/bin:/opt/local/sbin'
     }
     default: {
+      $illumos = false
       $oracle = true
-      $path = "/usr/gnu/bin/amd64:/usr/gnu/bin:/usr/bin/amd64:/usr/bin:/usr/X11/bin/amd64:/usr/X11/bin:/usr/sbin/amd64:/usr/sbin:/sbin"
+      $path = '/usr/gnu/bin/amd64:/usr/gnu/bin:/usr/bin/amd64:/usr/bin:/usr/X11/bin/amd64:/usr/X11/bin:/usr/sbin/amd64:/usr/sbin:/sbin'
     }
   }
 }
