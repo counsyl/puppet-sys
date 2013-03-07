dcaed8adf202b8cbdd146b644b7ace624a9d8623
Justin Bronn <justin@counsyl.com>
2013-03-06 18:53:34 -0800

    Fixed `sys::ubuntu::build_essential`.

--- manifests/ubuntu/build_essential.pp
+++ manifests/ubuntu/build_essential.pp
@@ -1,17 +1,25 @@
-class sys::ubuntu::build-essential {
+# == Class: sys::ubuntu::build_essential
+#
+# Installs GCC and kernel headers, the essentials necessary for
+# building Ubuntu kernel modules.
+#
+class sys::ubuntu::build_essential {
   include sys::gcc
   package { 'build-essential':
     ensure  => installed,
     require => Class['sys::gcc'],
   }
 
-  # Debian and Ubuntu have different package names for
-  # the Linux kernel headers.
-  if $::lsbdistid == Debian {
-    $kernel_headers = "linux-headers-${kernelrelease}"
+  # Debian and Ubuntu have different names for kernel headers package.
+  if $::lsbdistid == 'Debian' {
+    $kernel_flavor = $::kernelrelease
   } else {
-    $kernel_headers = 'linux-headers-server'
+    # Have to extract the "flavor" of the kernel from the $::kernelrelease
+    # fact. For example: '2.6.32-40-generic' => 'generic'
+    #                    '3.2.0-38-virtual'  => 'virtual'
+    $kernel_flavor = regsubst($::kernelrelease, '^.+\-(\w+)$', '\1')
   }
+  $kernel_headers = "linux-headers-${kernel_flavor}"
 
   package { $kernel_headers:
     ensure => installed,
