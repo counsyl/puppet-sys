37068cc5c5d31c4ec2ebb498f987d903aaf74b85
Justin Bronn <justin@counsyl.com>
2013-04-25 11:38:09 -0700

    Removed `alias` parameter to prevent metaparameter clash.

--- manifests/wget.pp
+++ manifests/wget.pp
@@ -4,14 +4,13 @@
 #
 class sys::wget (
   $ensure   = 'installed',
-  $alias    = 'wget',
   $package  = $sys::wget::params::package,
   $source   = $sys::wget::params::source,
   $provider = $sys::wget::params::provider,
 ) inherits sys::wget::params {
   package { $package:
     ensure   => $ensure,
-    alias    => $alias,
+    alias    => 'wget',
     provider => $provider,
     source   => $source,
   }
