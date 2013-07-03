9a366566f53c5e6b251f51a5dc168d1f21f63f1d
Justin Bronn <justin@counsyl.com>
2013-07-03 15:31:55 -0700

    Fixed incorrect wget parameters and added `path` variable.

--- manifests/wget/params.pp
+++ manifests/wget/params.pp
@@ -3,15 +3,22 @@
 # Platform-dependent parameters for wget.
 #
 class sys::wget::params {
-  $package = 'wget'
   case $::osfamily {
     openbsd: {
       include sys::openbsd::pkg
+      $package = 'wget'
+      $path = '/usr/local/bin/wget'
       $source = $openbsd::pkg::source
     }
     solaris: {
       include sys::solaris
+      $package = 'web/wget'
+      $path = '/usr/bin/wget'
       $provider = 'pkg'
+    }
+    default: {
+      $package = 'wget'
+      $path = '/usr/bin/wget'
     }
   }
 }
