85566eb60a6f141d42854d0a27f6e5c94cc8070d
Justin Bronn <justin@counsyl.com>
2015-03-23 23:49:07 -0500

    Improvments to `sys::openbsd::dnsmasq`: * Now take same type of hash as `sys::openbsd::hosts`, and generate `/etc/hosts` with it. * Added parameters for explicitly setting which interface/address to listen (or not listen on). * Added docstrings explaining the format of the network hash, and give an example. * No longer try and modify rc.conf.local, wait for Puppet's OpenBSD service support for rcctl in 5.7. * Validate parameters and improve ERB template syntax.

--- manifests/openbsd/dnsmasq.pp
+++ manifests/openbsd/dnsmasq.pp
@@ -1,30 +1,85 @@
 # == Class: sys::openbsd::dnsmasq
 #
-# Installs a simple DNS server/forwarder and DHCP service from the
-# given network parameter hash.
+# Installs a simple DNS server/forwarder and DHCP services from the given
+# network parameter hash.  In particular, this class will populate
+# /etc/hosts and generate /etc/dnsmasq.conf dynamically from the given
+# host information in the networks parameter (described below).
+#
+# === Parameters
+#
+# Most parameters correspond directly to DNSmasq configuration options.
+#
+# [*networks*]
+#  A hash containing the information for the networks you want to serve.  Each
+#  key corresponds to a network, which is another hash with the information
+#  necessary to run DNS and DHCP services for that network.  The hostnames and
+#  IP addresses are kept in a 'hosts' subkey, with the DHCP ranges kept in a
+#  'ranges' subkey.  The hosts hash maps hostnames to IP addresses (contained in
+#  a 'ip' key) and MAC addresses (optional, contained in a 'mac' key).
+#
+# === Example
+#
+# The following would create add two hosts, ldap and www, to /etc/hosts
+# and configure dnsmasq.conf to assign the IP to the MAC address for
+# ldap and for any DHCP client identifying as www (since no MAC was
+# specified).  The range from 192.168.0.50-100 is used for dynamic
+# leases:
+#
+#   class { 'sys::openbsd::dnsmasq':
+#     networks => {
+#       'servers' => {
+#         'hosts' => {
+#           'ldap' => {
+#             'ip'  => '192.168.0.10',
+#             'mac' => '00:80:de:ad:be:ef',
+#           },
+#           'www' => {
+#             'ip'  => '192.168.0.80',
+#           }
+#         },
+#         'ranges' => [ '192.168.0.50', '192.168.0.100' ],
+#       }
+#     },
+#     domain     => 'servers.counsyl.com',
+#     forwarders => [ '192.168.10.1', '192.168.10.2' ],
+#   }
 #
 class sys::openbsd::dnsmasq(
   $networks,
-  $config        = '/etc/dnsmasq.conf',
-  $cache_size    = '750',
-  $domain        = "${::domain}",
-  $expand_hosts  = true,
-  $forwarders    = ['8.8.8.8', '8.8.4.4'],
-  $default_lease = '24h',
-  $rc_local      = '/etc/rc.conf.local',
-  $rc_settings   = {
-    'name'    => 'pkg_scripts',
-    'value'   => 'dnsmasq',
-    'comment' => 'Start third-party package scripts, like dnsmasq.',
-  },
-  $template      = 'sys/openbsd/dnsmasq.conf.erb',
-  $extra         = undef,
+  $authoritative      = false,
+  $config             = '/etc/dnsmasq.conf',
+  $cache_size         = '750',
+  $default_lease      = '24h',
+  $domain             = "${::domain}",
+  $expand_hosts       = true,
+  $etc_hosts          = '/etc/hosts',
+  $except_interfaces  = [],
+  $forwarders         = ['8.8.8.8', '8.8.4.4'],
+  $interfaces         = [],
+  $listen_addresses   = [],
+  $no_dhcp_interfaces = [],
+  $package            = 'dnsmasq',
+  $service            = 'dnsmasq',
+  $service_enable     = true,
+  $service_ensure     = 'running',
+  $template           = 'sys/openbsd/dnsmasq.conf.erb',
+  $extra              = false,
 ) {
   include sys::openbsd::pkg
 
-  package { 'dnsmasq':
+  validate_hash($networks)
+  validate_array(
+    $except_interfaces, $forwarders,
+    $interfaces, $listen_addresses,
+    $no_dhcp_interfaces
+  )
+
+  package { $package:
     ensure => installed,
-    source => $sys::openbsd::pkg::source,
+  }
+
+  sys::openbsd::hosts { $etc_hosts:
+    networks => $networks,
   }
 
   file { $config:
@@ -33,21 +88,15 @@ class sys::openbsd::dnsmasq(
     group   => 'wheel',
     mode    => '0644',
     content => template($template),
-    require => Package['dnsmasq'],
+    require => [Package[$package], File[$etc_hosts]],
   }
 
-  if $rc_local {
-    sys::openbsd::rc { $rc_local:
-      settings => [$rc_settings],
-      require  => File[$config],
+  if $service {
+    service { $service:
+      enable    => $service_enable,
+      ensure    => $service_ensure,
+      subscribe => File[$config, $etc_hosts],
+      require   => Package[$package],
     }
-  }
-
-  service { 'dnsmasq':
-    ensure    => 'running',
-    provider  => 'base',
-    start     => '/usr/local/sbin/dnsmasq',
-    subscribe => File[$config],
-    require   => [Package['dnsmasq'], File[$config]],
   }
 }
